79c12203484b4e7df5e8073aa1c1fe58d467f52d
Constantina <polyccon@gmail.com>
2018-03-31 20:02:47 +0100

    fixes initial state of weapons in tests

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,3 +1,28 @@
 import pytest
 
 from src.game import update_state
+
+@pytest.mark.parametrize(
+    ("moves", "expected"),
+    [
+        ([{'player': 'red', 'dir': 'S'},
+        {'player': 'red', 'dir': 'S'},
+        {'player': 'red', 'dir': 'E'},
+        {'player': 'red', 'dir': 'E'}],
+        {
+            'red': [(2,2),'LIVE','axe',3,1],
+            'blue': [(7, 0), 'LIVE', None, 1,1],
+            'green': [(7,7),'LIVE',None,1,1],
+            'yellow': [(0, 7), 'LIVE', None, 1, 1],
+            'magic_staff': [(5,2),False],
+            'helmet': [(5,5),False],
+            'dagger': [(2,5),False],
+            'axe': [(2,2),True],
+        })
+    ]
+)
+def test_is_equal(moves, expected):
+    """Test if red player gets the axe as expected
+    """
+    actual = update_state(moves)
+    assert actual == expected
--- tests/test_2.py
+++ tests/test_2.py
@@ -15,10 +15,10 @@ from src.game import update_state
             'blue': [(7, 1), 'LIVE', None, 1, 1],
             'green': [(6,7),'LIVE',None,1,1],
             'yellow': [(-1, 7), 'DROWNED', None, 0, 0],
-            'magic_staff': [(2,2),False],
-            'helmet': [(5,2),False],
-            'dagger': [(5,5),False],
-            'axe': [(2,5),False],
+            'magic_staff': [(5,2),False],
+            'helmet': [(5,5),False],
+            'dagger': [(2,5),False],
+            'axe': [(2,2),False],
         })
     ]
 )
