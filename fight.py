344b678eba6ee7036748fac5a5a7331b62aab8df
Constantina <polyccon@gmail.com>
2018-03-31 10:12:14 +0100

    adds fight clean up

--- fight.py
+++ fight.py
@@ -1,18 +1,3 @@
-moves = [{'player': 'red', 'dir': 'S'}, {'player': 'red', 'dir': 'S'},
-        {'player':'blue', 'dir': 'E'}, {'player': 'green', 'dir': 'N'}]
-
-state= {
-    'red': [(0,0),'LIVE',None,1,1],
-    'blue': [(7,0),'LIVE',None,1,1],
-    'green': [(7,7),'LIVE',None,1,1],
-    'yellow': [(0,7),'LIVE',None,1,1],
-    'magic_staff': [(2,2),False],
-    'helmet': [(5,2),False],
-    'dagger': [(5,5),False],
-    'axe': [(2,5),False],
-}
-
-
 def fight(move, state):
     for defender in ['red', 'blue', 'green', 'yellow']:
         if defender == move['player']:
@@ -23,5 +8,5 @@ def fight(move, state):
             if state[move['player']][3] > state[defender][4]:
                 state[defender][1] == 'DEAD'
             elif state[move['player']][3] < state[defender][4]:
-                state[move['player']] == 'DEAD'
+                state[move['player']][1] == 'DEAD'
     return state
